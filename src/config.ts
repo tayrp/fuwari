425335e3361cb974d88744d8eb48c05edaac528f
tayrp <valerie@valerie.lol>
2026-01-24 18:58:27 -0500

    Update banner

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,13 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/estella_banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
 			text: "Pixiv", // Credit text to be displayed
-			url: "https://www.pixiv.net/en/artworks/140099618", // (Optional) URL link to the original artwork or artist's page
+			url: "https://www.pixiv.net/en/artworks/138350748",
+			// url: "https://www.pixiv.net/en/artworks/140099618", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
